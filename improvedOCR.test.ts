ac4a5c19345c0a4d2f5007cd8cb3ff94243dadd6
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-02-10 22:04:48 +0100

    :rocket:

--- improvedOCR.test.ts
+++ improvedOCR.test.ts
@@ -1,12 +1,18 @@
 import {ImprovedOCR} from "./improvedOCR";
 
-describe('description',()=>{
+const str = "  _  _     _  _  _  _  _ \n" +
+            "| _| _||_||_ |_   ||_||_|\n" +
+            "||_  _|  | _||_|  ||_| _|\n";
+
+const filePath = "./data.txt";
+describe('all tests',()=>{
     test('should be true',()=>{
         expect(true).toBe(true);
     });
 
     test('should be the same',()=>{
-        const improvedOCR = new ImprovedOCR("vide")
-        expect(improvedOCR.filePath).toBe("vide");
+        const improvedOCR = new ImprovedOCR(filePath)
+        console.log(str);
+        expect(improvedOCR.filePath).toBe(filePath);
     })
 });
