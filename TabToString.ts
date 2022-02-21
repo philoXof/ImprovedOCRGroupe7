5b4d18e8c3b17f802107186e85dc88b6bbfed8db
xof <ljehanno@myges.fr>
2022-02-21 23:47:31 +0100

    :art: refactoring

--- TabToString.ts
+++ TabToString.ts
@@ -1,76 +1,57 @@
 export class TabToString {
+    private keymap : Map<string,string[]> = new Map([
+        ["0" , [
+            " _ ",
+            "| |",
+            "|_|",]],
+        ["1",[
+            "   ",
+            "  |",
+            "  |",]],
+        ["2",[
+            " _ ",
+            " _|",
+            "|_ ",]],
+        ["3" ,[
+            " _ ",
+            " _|",
+            " _|"]],
+        ["4" ,[
+            "   ",
+            "|_|",
+            "  |"]],
+        ["5" ,[
+            " _ ",
+            "|_ ",
+            " _|"
+            ]],
+        ["6" , [
+            " _ ",
+            "|_ ",
+            "|_|"
+            ]],
+        ["7" ,[
+            " _ ",
+            "  |",
+            "  |"
+            ]],
+        ["8" ,[
+            " _ ",
+            "|_|",
+            "|_|"
+            ]],
+        ["9" ,[
+            " _ ",
+            "|_|",
+            " _|"
+            ]]
+    ]);
 
-    private zero : string[] = [
-        " _ ",
-        "| |",
-        "|_|",
-    ];
-    private one : string[]  = [
-        "   ",
-        "  |",
-        "  |",
-    ];
-    private two : string[]  = [
-        " _ ",
-        " _|",
-        "|_ ",
-    ];
-    private tree : string[]  = [
-        " _ ",
-        " _|",
-        " _|"
-    ];
-    private four : string[]  = [
-        "   ",
-        "|_|",
-        "  |"
-    ];
-    private five : string[]  = [
-        " _ ",
-        "|_ ",
-        " _|"
-    ];
-    private six : string[]  = [
-        " _ ",
-        "|_ ",
-        "|_|"
-    ];
-    private seven : string[]  = [
-        " _ ",
-        "  |",
-        "  |"
-    ];
-    private height : string[]  = [
-        " _ ",
-        "|_|",
-        "|_|"
-    ];
-    private nine : string[] = [
-        " _ ",
-        "|_|",
-        " _|"
-    ];
-
-    //ajouter string en retour pour renvoyer
-    public tabToString(tab : string[]) : string {
-        for (let [key,value] of this.keymap) if(JSON.stringify(value) == JSON.stringify(tab)) return key;
-        return "0";
+    public convert(tab : string[]) : string {
+        for (let [key,value] of this.keymap)
+            if(JSON.stringify(value) == JSON.stringify(tab))
+                return key;
+        return "?";
     }
-
-    private keymap : Map<string,string[]> = new Map<string, string[]>();
-
-    constructor() {
-        this.keymap.set("0", this.zero);
-        this.keymap.set("1", this.one);
-        this.keymap.set("2", this.two);
-        this.keymap.set("3", this.tree);
-        this.keymap.set("4", this.four);
-        this.keymap.set("5", this.five);
-        this.keymap.set("6", this.six);
-        this.keymap.set("7", this.seven);
-        this.keymap.set("8", this.height);
-        this.keymap.set("9", this.nine);
-    }
-
 
 }
--- improvedOCR.ts
+++ improvedOCR.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { TabToString } from "./TabToString";
+
 export class ImprovedOCR{
     private readonly filePath : string;
 
@@ -7,67 +8,33 @@ export class ImprovedOCR{
         this.filePath = filePath;
     }
 
-    public getFileData() : string {
+    public getFileContent() : string {
         return  fs.readFileSync(this.filePath, {encoding:'utf-8'});
     }
 
-
     public decodeFile() : string {
         const tabToString : TabToString = new TabToString();
-        const tab : string[] = this.createAndInitTab();
-
+        const tab : string[] = this.fileToTab();
         let code : string = "";
-        for (let i = 0; i <= 24; i += 3) {
-            const tempTab : string[] = [tab[0].slice(i, i + 3), tab[1].slice(i, i + 3), tab[2].slice(i, i + 3)];
-            code += tabToString.tabToString(tempTab);
-        }
-
+        for (let i = 0; i <= 24; i += 3)
+            code += tabToString.convert([tab[0].slice(i, i + 3), tab[1].slice(i, i + 3), tab[2].slice(i, i + 3)]);
         return code;
     }
 
-    public createAndInitTab() : string[] {
-        let fileContent : string = this.getFileData();
-
+    public fileToTab() : string[] {
+        let fileContent : string = this.getFileContent();
         let tab : string[] = ["","",""];
 
         for (let i = 0, j = 0; i <= 2; i++) {
-            let temp = "";
-            for (; fileContent[j] != '\r'; j++) {
-                temp +=  fileContent[j];
-            }
-            j++;
-            j++;
-            if(temp.length < 27){
-                while (temp.length < 27) temp += " ";
-            }
-            tab[i] = temp;
+            let line = "";
+            for (; fileContent[j] != '\r'; j++)
+                line +=  fileContent[j];
+            j += 2;
+            if(line.length < 27)
+                while (line.length < 27)
+                    line += " ";
+            tab[i] = line;
         }
         return tab;
     }
-
-    /*private static printTab1(tab : Array<string>) : void {
-        for (let i = 0; i <= 2; i++) {
-            console.log(tab[i]);
-        }
-    }*/
-
-    /**
-     * try with tab 2 dimensions
-
-    private static printTab(tab : Array<Array<string>>) : void {
-        for (let i = 0; i <= tab.length; i++) {
-            for (let j = 0; j <= 25; j++) {
-                console.log(i, j, tab[i][j]);
-            }
-        }
-    }
-
-    private static initTab(tab : Array<Array<string>>) : Array<Array<string>> {
-        for (let i = 0; i <= tab.length; i++) {
-            for (let j = 0; j <= 25; j++) {
-                tab[i][j] = "d";
-            }
-        }
-        return tab;
-    }*/
 }
